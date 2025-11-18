35477f1bc4242edb88256c344a583ab7ded18336
Keanu Liwongan <keanu.liwongan@gmail.com>
2025-11-18 00:14:45 -0700

    Moved main Raft operating logic to RaftService

--- src/raft.rs
+++ src/raft.rs
@@ -1,6 +1,19 @@
 use rand::Rng;
 use std::cmp::min;
 use std::collections::{HashMap, HashSet};
+use std::io::Error;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::runtime::{Handle, Runtime};
+use tokio::sync::Mutex;
+use tokio::time::timeout;
+
+use tracing::{Level, error, info};
+use tracing_subscriber::FmtSubscriber;
 
 use serde::{Deserialize, Serialize};
 
@@ -69,7 +82,7 @@ struct RequestVote {
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
-struct RequestVoteReply {
+pub struct RequestVoteReply {
     term: u32,
     voteGranted: bool,
     _id: u32,
@@ -161,7 +174,7 @@ impl RaftService {
             // existing entry in the same index but conflicts with current term
             if entry.term != args.prevLogTerm {
                 // find first index and entry term of conflicting entries
-                
+
                 // TODO
                 result.conflictingEntryTerm = Some(1);
                 result.conflictingFirstIndex = Some(1);
@@ -236,7 +249,7 @@ impl RaftService {
                 nextIndex = newIndex - 1;
             }
             let prevLogTerm = if self.log.len() as u32 - 1 >= 0 {
-                self.log.get(nextIndex).unwrap().term
+                self.log.get(nextIndex as usize).unwrap().term
             } else {
                 self.currentTerm
             };
@@ -250,6 +263,8 @@ impl RaftService {
                 leaderCommit: self.commitIndex,
             });
         }
+        // Use voteState to maintain nodes that have already replicated our entries
+        self.voteState.insert(args._id);
         None
     }
 
@@ -327,6 +342,7 @@ impl RaftService {
     // NodeState related methods
 
     pub fn become_leader(&mut self) {
+        self.voteState.clear();
         self.state = NodeState::Leader;
     }
 
@@ -335,7 +351,9 @@ impl RaftService {
     }
 
     pub fn become_candidate(&mut self) {
+        self.incrementTerm();
         self.state = NodeState::Candidate;
+        self.voteState.insert(self._id);
     }
 
     pub fn setTerm(&mut self, term: u32) {
@@ -344,6 +362,10 @@ impl RaftService {
 
     pub fn getTerm(&self) -> u32 {
         return self.currentTerm;
+    }
+
+    pub fn incrementTerm(&mut self) {
+        self.currentTerm = self.currentTerm + 1;
     }
 
     pub fn is_leader(&self) -> bool {
@@ -361,4 +383,104 @@ impl RaftService {
     pub fn add_to_network(&mut self, node: u32) {
         self.network.nodes.insert(node);
     }
+
+    pub async fn run(&mut self) {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 2222));
+        let listener = TcpListener::bind(&addr).await.unwrap();
+        let node = Arc::new(Mutex::new(RaftService::new(2222)));
+
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(Level::TRACE)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
+
+        info!("Listening on: http://{}", addr);
+        loop {
+            info!("At loop start!");
+            let cur_node = Arc::clone(&node);
+            let mut node_lock = cur_node.lock().await;
+            if node_lock.is_follower() {
+                info!("Node is a follower");
+                let timeout_duration = Duration::from_millis(node_lock.get_timeout());
+                let result = timeout(timeout_duration, listener.accept()).await;
+                match result {
+                    Err(_) => {
+                        info!("Becoming a candidate because timeout was reached");
+                        node_lock.become_candidate();
+                        node_lock.reset_timeout();
+                        drop(node_lock);
+                        continue;
+                    }
+                    Ok(result) => {
+                        if let Err(_) = result {
+                            error!("Error in receiving RPC");
+                            // error with receiving the RPC
+                            continue;
+                        } else {
+                            let rt = Runtime::new().unwrap();
+                            let handle = rt.handle();
+                            drop(node_lock);
+                            handle_request(cur_node, result, handle.clone());
+                        }
+                    }
+                };
+            } else if node_lock.is_candidate() {
+                info!("Node is a candidate");
+                let timeout_duration = Duration::from_millis(node_lock.get_timeout());
+                // let result = timeout(timeout_duration, listener.accept()).await;
+                while !node_lock.check_majority() {
+                    //RaftService::parse_response(node_lock.send_request_vote());
+                }
+            } else {
+                // node is leader
+
+                // await client requests
+
+                // if client requests to see something, return the connection within its log
+                // else,
+            }
+        }
+    }
+}
+
+fn handle_basic_http_request(
+    result: Result<(TcpStream, SocketAddr), Error>,
+    handle: Handle,
+) {
+    // dummy function for testing purposes
+    let (mut stream, mut address) = result.unwrap();
+    handle.spawn(async move {
+        info!("Serving request!");
+        let mut buffer = [0; 1024];
+        let _ = stream.read(&mut buffer).await;
+
+        let contents = "<h1>Hello, world!</h1>";
+        let content_length = contents.len();
+        let response =
+            format!("HTTP/1.1 200 OK\r\nContent-Length: {content_length}\r\n\r\n{contents}");
+        let _ = stream.write_all(response.as_bytes()).await;
+    });
+}
+
+async fn handle_request(
+    node: Arc<Mutex<RaftService>>,
+    result: Result<(TcpStream, SocketAddr), Error>,
+    handle: Handle,
+) {
+    let (mut stream, mut address) = result.unwrap();
+    handle.spawn(async move {
+        let cur_node = Arc::clone(&node);
+        let mut node_lock = cur_node.lock().await;
+        info!("Deserializing request!");
+        let mut buffer = [0; 1024];
+        let _ = stream.read(&mut buffer).await;
+
+        let message = String::from_utf8(buffer.to_vec()).unwrap();
+        info!("{}", format!("{}: {}", "Message", &message));
+        let response = node_lock.execute_from_message(&message);
+
+        // now write the response back
+        let _ = stream.write_all(response.as_bytes()).await;
+    });
 }
